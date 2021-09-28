451bb8afbc6385675ad0e7377e820a287cc12325
Ali BUtt <alibutt7867@gmail.com>
2021-09-28 23:59:06 +0500

    1st

--- src/Reactstrap/Header/Header.js
+++ src/Reactstrap/Header/Header.js
@@ -1,38 +1,44 @@
 import React,{useState} from 'react';
 import styles from './Header.module.css'; 
 const Header = () => {
+  // Initial goalList
 const titles = [ ];
 const [input, setinput] = useState('');
 const [goals, setsgoals] = useState(titles);
 const [goals1, setsgoals1] = useState([]);
-
+//AddGoal
 const addTranssection = e=>{
 e.preventDefault();
 if(input!==''){
 goals.push({title:input})
 }setinput('');  }
+//Delete Goal
 const del=(e)=>{
 setsgoals(goals.filter((item,ind) => ind !== e));
   }
+  //Edit Goal
   const edit=(e,p)=>{
     setinput(e.title)
     setsgoals1(p);
+   const pp= document.getElementById('pak').style.visibility="visible"
+   document.getElementById('pak1').style.display="none"
+    console.log(pp);
       }
+      //Update Goal
       const update=e=>{
         e.preventDefault();
         if(input!==''){
           goals[goals1].title=input;
         setsgoals([...goals])}
         setinput('');
-
-      
-          }
+        document.getElementById('pak').style.visibility="hidden" 
+        document.getElementById('pak1').style.display="inline" }
   return (
     <div style={{marginTop:'50px'}}>
     <form >
          <input name="firstName" placeholder="Your Title" value={input}   onChange={(e) => setinput(e.target.value)} />
-            <button onClick={addTranssection} >add transsection</button>
-            <button className={styles.update} onClick={update} >Update</button>
+            <button  id='pak1' className={styles.addTranssection} onClick={addTranssection} >add transsection</button>
+            <button id='pak' className={styles.update} onClick={update} >Update</button>
             </form>   
        <br/>  
        <br/>  
